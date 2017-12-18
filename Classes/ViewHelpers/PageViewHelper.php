ed39b5ee7aa311d956493e889a05578a4a52971f
Marvin Kuhn <marvin.kuhn92@gmail.com>
2017-12-18 16:02:02 +0100

    dimension support added

--- Classes/ViewHelpers/PageViewHelper.php
+++ Classes/ViewHelpers/PageViewHelper.php
@@ -6,47 +6,84 @@ use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
 use Neos\Eel\FlowQuery\FlowQuery;
 use Neos\Flow\Annotations as Flow;
 use Neos\Neos\View\FusionView;
+use Neos\Neos\Routing\FrontendNodeRoutePartHandler;
+
 
 class PageViewHelper extends AbstractViewHelper
 {
+    /**
+     * error page type name
+     */
     const ERROR_PAGE_TYPE = 'Breadlesscode.ErrorPages:Page';
     /**
      * @Flow\Inject
      * @var ContextFactoryInterface
      */
     protected $contextFactory;
-
+    /**
+     * @Flow\InjectConfiguration(path="contentDimensions.language", package="Neos.ContentRepository")
+     * @var array
+     */
+    protected $contentDimensionsConfig = [];
+    /**
+     * @Flow\InjectConfiguration(path="routing.supportEmptySegmentForDimensions", package="Neos.Neos")
+     * @var boolean
+     */
+    protected $supportEmptySegmentForDimensions;
+    /**
+     * @param  Neos\Flow\Mvc\Controller\Exception\InvalidControllerException $exception
+     * @return string
+     */
     public function render($exception)
     {
         $statusCode = $exception->getStatusCode();
-        $request = $this->controllerContext->getRequest();
-        $errorPages = $this->getErrorPages();
-        $pageToRender = null;
-        // find the correct error page
-        foreach ($errorPages as $errorPage) {
-            $supportedStatusCodes = $errorPage->getProperty('statusCodes');
-            if(!$supportedStatusCodes !== null && \in_array($statusCode, $supportedStatusCodes))
-                $pageToRender = $errorPage;
-        }
+        $dimension = $this->getCurrentDimension();
+        $errorPage = $this->findErrorPage($statusCode, $dimension);
 
-        if($pageToRender === null)
+        if($errorPage === null) {
             throw new \Exception("Please setup a error page of type ".self::ERROR_PAGE_TYPE."!", 1);
-
+        }
         // render error page
         $view = new FusionView();
         $view->setControllerContext($this->controllerContext);
-        $view->assign('value', $pageToRender);
+        $view->setFusionPath('default');
+        $view->assign('value', $errorPage);
 
         return $view->render();
+    }
+    /**
+     * find error page in a specific dimension with status code xy in it
+     *
+     * @param  mixed $statusCode
+     * @param  string $dimension
+     * @return Neos\ContentRepository\Domain\Model\Node
+     */
+    protected function findErrorPage($statusCode, $dimension)
+    {
+        $errorPages = collect($this->getErrorPages($dimension));
+        $statusCode = (string) $statusCode;
+        // find the correct error page
+        return $errorPages
+            // filter invalid status codes
+            ->filter(function($page) use($statusCode) {
+                $supportedStatusCodes = $page->getProperty('statusCodes');
+
+                return $supportedStatusCodes !== null && \in_array($statusCode, $supportedStatusCodes);
+            })
+            // filter invalid dimensions
+            ->filter(function($page) use($dimension) {
+                return \in_array($dimension, $page->getDimensions()['language']);
+            })
+            ->first();
     }
     /**
      * collects all error pages from the site
      *
      * @return array    collection of error pages
      */
-    protected function getErrorPages()
+    protected function getErrorPages($dimension)
     {
-        $context = $this->getContext();
+        $context = $this->getContext($dimension);
 
         return (new FlowQuery([ $context->getNode('/') ]))
             ->find('[instanceof '. self::ERROR_PAGE_TYPE .']')
@@ -57,16 +94,49 @@ class PageViewHelper extends AbstractViewHelper
      *
      * @return Neos\ContentRepository\Domain\Service\Context
      */
-    protected function getContext()
+    protected function getContext($dimension)
     {
         return $this->contextFactory->create([
             'workspaceName' => 'live',
             'currentDateTime' => new \Neos\Flow\Utility\Now(),
-            'dimensions' => [],
+            'dimensions' => ['language' => [$dimension, $this->contentDimensionsConfig['defaultPreset']]],
+            'targetDimensions' =>  ['language' => $dimension],
             'invisibleContentShown' => true,
             'removedContentShown' => false,
             'inaccessibleContentShown' => false
         ]);
+    }
+    /**
+     * get the current dimension preset key
+     *
+     * @return string   dimension preset key
+     */
+    protected function getCurrentDimension()
+    {
+        $matches = [];
+        $requestPath = ltrim($this->controllerContext->getRequest()->getHttpRequest()->getUri()->getPath(), '/');
+        preg_match(FrontendNodeRoutePartHandler::DIMENSION_REQUEST_PATH_MATCHER, $requestPath, $matches);
+
+        $presets = collect($this->contentDimensionsConfig['presets'])
+            ->filter(function($value, $key) use($matches) {
+                $uriSegment = data_get($value, 'uriSegment');
+                return (
+                    $uriSegment !== null && (
+                        $uriSegment === data_get($matches, 'firstUriPart') ||
+                        $uriSegment === data_get($matches, 'dimensionPresetUriSegments')
+                    )
+                );
+            });
+
+        if($presets->count() > 0) {
+            return $presets->keys()->first();
+        }
+
+        if($this->supportEmptySegmentForDimensions) {
+            return $this->contentDimensionsConfig['defaultPreset'];
+        }
+
+        return null;
     }
 
 }
